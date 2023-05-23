62ff8f6d1a805135f81acadd63b65bd8cc87e74b
Salma <salmaelaadam@gmail.com>
2023-05-23 18:30:25 +0100

    update header file

--- shell.h
+++ shell.h
@@ -4,7 +4,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#define BUFFER 1024
 
 void print_string(char *string);
+char **token(char *stg);
+void prompt(int file, struct stat buffer);
+void _puts(char *string);
+void create_cld(char **cmd, char *nm, har **env, int cc);
+int change_direct(const char *dest);
 
 #endif
